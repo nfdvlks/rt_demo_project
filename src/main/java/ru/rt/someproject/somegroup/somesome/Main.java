e51d41092c11df034a5138a80ddd2234fd0cac5f
anefedyev <anefedyev@mail.ru>
2025-04-27 12:03:17 +0300

    Тестовое задание. Первый и единственный коммит.

---
+++ src/main/java/ru/rt/someproject/somegroup/somesome/Main.java
@@ -0,0 +1,9 @@
+package ru.rt.someproject.somegroup.somesome;
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        new EmulatorThreadResourceWorking();
+    }
+}
